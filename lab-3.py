2ab4206fa3fd641a96d51448d1a8498e8ca7566e
Dan Hardej <danielhardej@gmail.com>
2022-08-19 14:38:12 +0100

    Lab 3 code issues resolved

--- lab-3.py
+++ lab-3.py
@@ -7,21 +7,23 @@ s3 = boto3.client('s3')
 
 def main(argv):
     cmd = argv
-    log_file_name = datetime.datetime.now(tzinfo=UTC).strftime("%m_%d_%Y") + "_logfile"
+    log_file_name = datetime.datetime.now(tzinfo=datetime.timezone.utc).strftime("%m_%d_%Y") + "_logfile"
     kickoff_subprocess(cmd, log_file_name)
     upload_output_to_S3(log_file_name)
 
 def kickoff_subprocess(cmd, log_file_name):
     process = subprocess.call(cmd, shell=False)
-    file = open(log_file_name, "a+")
-    timestamp = datetime.datetime.now(tzinfo=UTC).strftime("%m/%d/%Y, %H:%M:%S")
-    output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
+    # not needed as with open() used
+    # file = open(log_file_name, "a+")
     with open(log_file_name, 'a+') as file:
+        timestamp = datetime.datetime.now(tzinfo=atetime.timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
+        output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
         file.write(output)
 
 def upload_output_to_S3(log_file_name):
-    with open(log_file_name, 'a+') as file:
-        file = open(log_file_name, "rb")
+    with open(log_file_name, 'rb') as file:
+        # not needed as with open() used
+        # file = open(log_file_name, "rb")
         s3.upload_fileobj(file, "<FMI1>", log_file_name)
         file.close()
 
