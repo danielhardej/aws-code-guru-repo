9699213ddbe36ee6fd3b36b6fec7c3f2f900d4f3
Dan Hardej <danielhardej@gmail.com>
2022-08-19 15:10:31 +0100

    Updated lab 3 code

--- lab-3.py
+++ lab-3.py
@@ -6,7 +6,6 @@ import datetime
 s3 = boto3.client('s3')
 
 def main(argv):
-
     cmd = determine_command(int(sys.argv[1]))
     log_file_name = datetime.datetime.now(datetime.timezone.utc).strftime("%m_%d_%Y") + "_logfile"
     kickoff_subprocess(cmd, log_file_name)
@@ -28,18 +27,14 @@ def determine_command(user_input):
         return ['echo', "default"]
 
 def kickoff_subprocess(cmd, log_file_name):
-    #Linux
     process = subprocess.call(cmd, shell=False)
 
-    #Windows
-    #process = subprocess.call(['cmd', '/c'], shell=False)
-
-    file = open(log_file_name, "a+")
-    timestamp = datetime.datetime.now(datetime.timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
-    output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
     # context manager https://book.pythontips.com/en/latest/context_managers.html
     with open(log_file_name, 'a+') as file:
+        timestamp = datetime.datetime.now(datetime.timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
+        output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
         file.write(output)
+        file.close()
 
 def upload_output_to_S3(log_file_name):
     # context manager https://book.pythontips.com/en/latest/context_managers.html
